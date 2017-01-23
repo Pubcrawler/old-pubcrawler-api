6c857e4775399a0828ed0d364e39ca3e410328fe
John-Olav Storvold <johnolav.storvold@gmail.com>
2017-01-23 18:25:15 +0100

    Facebook Authentication and MongoDB connection

--- src/auth/facebook.js
+++ src/auth/facebook.js
@@ -1,1 +1,68 @@
-// TODO: Write facebook authentication endpoints here.
+import { Router } from 'express';
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import winston from 'winston';
+
+import User from '../db/models/user';
+
+
+export default (config) => {
+  const api = Router();
+
+  api.get('/facebook',
+    passport.authenticate('facebook'));
+
+  api.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/version' }), (req, res) => {
+    res.redirect('/');
+  });
+
+  passport.use(new FacebookStrategy({
+    clientID: config.facebook.appId,
+    clientSecret: config.facebook.secret,
+    callbackURL: config.facebook.callbackUrl,
+    display: 'popup',
+    authType: 'code',
+    passReqToCallback: 'true',
+    enableProof: true,
+    profileFields: ['id', 'displayName', 'gender', 'picture', 'emails'],
+  }, (request, accessToken, refreshToken, profile, callback) => {
+    const query = User.findOne({ id: profile.id }).exec();
+    winston.log('debug', 'facebook-profile:', profile);
+    query
+      .then((queriedUser) => {
+        if (!queriedUser) {
+          winston.log('debug', 'user is not registered.');
+          const newUser = new User({
+            name: profile.displayName,
+            id: profile.id,
+            gender: profile.gender,
+            picture: profile.photos ? profile.photos.value : null,
+            email: profile.emails ? profile.emails[0] : null,
+          });
+          newUser.save().then((savedUser) => {
+            winston.log('debug', 'user registered successfully');
+            return savedUser;
+          });
+        }
+        return queriedUser;
+      })
+      .then((returnedUser) => {
+        winston.log('debug', 'returning user');
+        return callback(null, returnedUser);
+      })
+      .catch((error) => {
+        winston.log('error', error);
+        return callback(error, null);
+      });
+  }));
+
+  passport.serializeUser((user, callback) => {
+    callback(null, user);
+  });
+
+  passport.deserializeUser((obj, callback) => {
+    callback(null, obj);
+  });
+
+  return api;
+};
